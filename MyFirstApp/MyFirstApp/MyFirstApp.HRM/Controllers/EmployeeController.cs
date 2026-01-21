6395a2c9f4afff53b9c979438cb3eb3f30e6b440
thngoc0000 <ngoctr2k4@gamil.com>
2026-01-21 10:46:14 +0700

    áas

---
+++ MyFirstApp/MyFirstApp/MyFirstApp.HRM/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstApp.HRM.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string SayHello()
+        {
+            return "Hello from EmployeeController!";
+        }
+    }
+}
---
+++ MyFirstApp/MyFirstApp/MyFirstApp.HRM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyFirstApp.HRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
